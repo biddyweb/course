3276c25a6ab8cf4d52a65c5bcc23a0d4b8df6d3e
Hatem Nassrat <hnassrat@gmail.com>
2007-11-07 20:13:52 -0400

    Sorting Messages and Downloads.

--- course/lib/helpers.py
+++ course/lib/helpers.py
@@ -11,15 +11,21 @@ def get_msg(msg_dir):
     Get all messages in msg_dir relevant to student with id=id
     '''
     msg_paths = [path.join(msg_dir, x) for x in listdir(msg_dir)]
-    msgs = []
-    for p in msg_paths:
-        f = open(p)
-        msgs.append(f.read())
-        f.close()
-    return "\n".join(msgs)
+    if msg_paths:
+        msg_paths.sort()
+        msgs = []
+        for p in msg_paths:
+            f = open(p)
+            msgs.append(f.read())
+            f.close()
+        return "\n".join(msgs)
+    return ""
 
 def get_fnames(dir):
     '''
     Get all downloadable file names in dir
     '''
-    return listdir(dir) 
+    # since sort is in place
+    files = listdir(dir)
+    files.sort() 
+    return files 
